5735cfb478e31246d4a7f66c72626d6429fafff6
dsolerh <dsolerh.cinter95@gmail.com>
2024-03-29 00:49:10 +0200

    chore: clean up unused routes

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
@@ -20,8 +15,6 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
         App::new()
             .route("/health-check", web::get().to(health_check))
             .route("/subscriptions", web::get().to(subscribe))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
     })
     .listen(listener)?
     .run();
