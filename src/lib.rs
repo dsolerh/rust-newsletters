bc2f43774b1b41df3d09129551e36529eec8b3ba
dsolerh <dsolerh.cinter95@gmail.com>
2024-03-29 01:09:06 +0200

    feat: parse the form data

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,23 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
+use actix_web::{
+    dev::Server,
+    web::{self, Form},
+    App, HttpResponse, HttpServer,
+};
+use serde::Deserialize;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-async fn subscribe() -> HttpResponse {
+#[derive(Deserialize)]
+struct SubscriptionData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: Form<SubscriptionData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
@@ -14,7 +25,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health-check", web::get().to(health_check))
-            .route("/subscriptions", web::get().to(subscribe))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
