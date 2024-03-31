3a161bc3cf9ccd5cde3b34eb976b1803f2cfa474
dsolerh <dsolerh.cinter95@gmail.com>
2024-03-31 14:37:18 +0300

    refactor: set the request id on a request logger base

--- src/startup.rs
+++ src/startup.rs
@@ -3,17 +3,17 @@ use std::net::TcpListener;
 use crate::routes::{health_check, subscribe};
 use actix_web::{
     dev::Server,
-    middleware::Logger,
     web::{self, Data},
     App, HttpServer,
 };
 use sqlx::PgPool;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let data = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health-check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(data.clone())
