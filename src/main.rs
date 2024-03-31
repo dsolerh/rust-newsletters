fd4865cf5661f7f17a60c0ed1e81b5d33ec7e39f
dsolerh <dsolerh.cinter95@gmail.com>
2024-03-31 11:52:55 +0300

    feat: switch from the env_logger to the tracing approach

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 use std::net::TcpListener;
 
-use env_logger::Env;
 use rust_newsletters::{config::get_config, startup::run};
 use sqlx::PgPool;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // Panic if we can't read configuration
     let config = get_config().expect("Failed to read configuration.");
 
-    // initialize the logger
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // initialize the tracing
+    LogTracer::init().expect("Failed to set logger");
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new(
+        "newsletters".into(),
+        // Output the formatted spans to stdout. std::io::stdout
+        std::io::stdout,
+    );
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber.");
 
     // initialize the connection pool
     let connection_pool = PgPool::connect(&config.database.connection_string())
